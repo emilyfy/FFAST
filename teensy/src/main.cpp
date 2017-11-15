99d22bef71b8d68c6b56f393a5903797fca55574
Jordan Ford <jsford94@gmail.com>
2017-11-14 22:04:47 -0500

    Added arduino libraries to read the Bosch accelerometer. Probably won't work if you don't have arduino and teensy installed at /usr/share/arduino. idk.

--- teensy/src/main.cpp
+++ teensy/src/main.cpp
@@ -1,12 +1,34 @@
-#include "WProgram.h"
+#include "BNO055/BNO055.h"
+#include <Arduino.h>
 
 int main(void)
 {
+    Serial.begin(115200);
 	pinMode(13, OUTPUT);
-	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(1000);
-		digitalWriteFast(13, LOW);
-		delay(1000);
-	}
+    digitalWriteFast(13, LOW);
+
+    Adafruit_BNO055 bno = Adafruit_BNO055();
+
+    if(!bno.begin())
+    {
+        digitalWriteFast(13, HIGH);
+        while (1);
+    }
+
+    delay(1000);
+    bno.setExtCrystalUse(true);
+
+    while(1) {
+       imu::Quaternion quat = bno.getQuat();
+       Serial.print("(");
+       Serial.print(quat.w(), 4);
+       Serial.print(", ");
+       Serial.print(quat.x(), 4);
+       Serial.print(", ");
+       Serial.print(quat.y(), 4);
+       Serial.print(", ");
+       Serial.print(quat.z(), 4);
+       Serial.print(")\n");
+       delay(10);
+    }
 }
