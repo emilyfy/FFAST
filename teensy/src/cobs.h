0e05987950e6c4cf23120391254a29e259b4f72f
Jordan Ford <jsford@andrew.cmu.edu>
2018-01-24 04:20:51 -0500

    This commit is gonna tick off emily. I screwed up a lot of her code. Am now using COBS to send data from teensy to Jetson. Publishing it to LCM.

---
+++ teensy/src/cobs.h
@@ -0,0 +1,14 @@
+/* Copyright 2011, Jacques Fortier. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted, with or without modification.
+ */
+#ifndef COBS_H
+#define COBS_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+size_t cobs_encode(const uint8_t * input, size_t length, uint8_t * output);
+size_t cobs_decode(const uint8_t * input, size_t length, uint8_t * output);
+
+#endif
